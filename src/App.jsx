c701f28451aef4842c53e361db3c4465f8f67a03
Vincent Veltri <vveltri@instructors.2u.com>
2024-10-30 21:04:10 -0400

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = 12
+const count = 12;
+
 function App() {
   const [count, setCount] = useState(0)
 
